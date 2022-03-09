6d05e532e186b8afb2c97a16613963215a0cff3e
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2022-03-09 22:23:10 +0300

    Update asa_aliceSendToBob.js

--- asa_aliceSendToBob.js
+++ asa_aliceSendToBob.js
@@ -3,17 +3,17 @@ const algosdk = require('algosdk');
 const server="https://testnet-algorand.api.purestake.io/ps2";
 const port="";
 const token={
-	"x-api-key": "d0tj2DSVGb28ppQa1MQpi9k35hArrNPP5PNLzXdU" // fill in yours
+	"x-api-key": "" // fill in yours
 };
 
-var alice_mnemonic = "mouse parade pond real rare mule metal vacant wire typical unfair celery canal beyond item almost daughter shiver arctic correct beyond cannon congress absent eternal"; // fill in yours
+var alice_mnemonic = ""; // fill in yours
 var aliceAccount = algosdk.mnemonicToSecretKey(alice_mnemonic);
-var bobAddress = 'K4NXOY7PNSMVKM34MGZULBEH47JLGT2JO2UA7OQTFM27BD73YOOE35M4MI'; // change to yours
+var bobAddress = ''; // change to yours
 
 let client = new algosdk.Algodv2(token, server, port);
 
 (async () => {
-    let assetID = 13300122; // change to your own assetID
+    let assetID = ; // change to your own assetID
     let params = await client.getTransactionParams().do();
     let sender = aliceAccount.addr;
     let recipient = bobAddress;
