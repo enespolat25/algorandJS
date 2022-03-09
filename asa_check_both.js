48c937148a47c788a957e9616151d29299453973
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2022-03-09 22:11:11 +0300

    Update asa_check_both.js

--- asa_check_both.js
+++ asa_check_both.js
@@ -3,13 +3,13 @@ const algosdk=require('algosdk');
 const server="https://testnet-algorand.api.purestake.io/ps2";
 const port="";
 const token={
-	"x-api-key": "d0tj2DSVGb28ppQa1MQpi9k35hArrNPP5PNLzXdU" // fill in yours
+	"x-api-key": "" // fill in yours
 };
 //asset-id= 76230366
-var aliceAddress = 'KWGRCHI5TX6VW7UHIAVIMHAOLE4YSSL3PC4CTYMKKSJ2CYEBSRLJEG6TXI'; // change to yours
-//alice mnemonic:"mouse parade pond real rare mule metal vacant wire typical unfair celery canal beyond item almost daughter shiver arctic correct beyond cannon congress absent eternal"
-var bobAddress = 'K4NXOY7PNSMVKM34MGZULBEH47JLGT2JO2UA7OQTFM27BD73YOOE35M4MI'; // change to yours
-//bob mnemonic="depth ahead gate lottery act bench damp sorry member allow alert text ball cargo diary uniform achieve dinner vapor taste scan kiwi season abandon finish"
+var aliceAddress = ''; // change to yours
+//alice mnemonic:""
+var bobAddress = ''; // change to yours
+//bob mnemonic=""
 
 let client=new algosdk.Algodv2(token, server, port);
 
